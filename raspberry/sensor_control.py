bfb06a86121f9c427d875c212b8dd64464f2a4da
Luis Cambero Piqueras <camberopiqueras@gmail.com>
2019-01-31 09:31:11 +0100

    Add DHT11 sensor

--- raspberry/sensor_control.py
+++ raspberry/sensor_control.py
@@ -1,7 +1,8 @@
-import pyrebase
-import serial
 import time
+import pyrebase
+import Adafruit_DHT
 
+#Set the configuration of the firebase realtime database
 firebaseConfig = {
   "apiKey": "AIzaSyB8jcGwrAajBJR9pyBNqZ3y7xweeyAdZA8",
   "authDomain": "dependentsassistant.firebaseapp.com",
@@ -9,17 +10,21 @@ firebaseConfig = {
   "storageBucket": "dependentsassistant.appspot.com"
 }
 
+# Initialize the firebase app
 firebase = pyrebase.initialize_app(firebaseConfig)
 
+# Set a reference to the database
 db = firebase.database()
 
-tv_onp = False
-tv_on = False
+# Configure the DHT11 temperature and humidity sensor
+dht11Sensor = Adafruit_DHT.DHT11
+dht11Pin = 4
 
 while True:
-	if tv_on != tv_onp:
-		tv_onp = tv_on
-		print(tv_on)
-
-	tv_on = db.child("tv/on").get().val()
-	#print(tv_on)
+	# Get the temperature and the humidity from the DHT11 sensor
+	humidity, temperature = Adafruit_DHT.read_retry(dht11Sensor, dht11Pin)
+	
+	# Post the temperature 
+	db.child('temperature').set(temperature)
+	
+	time.sleep(5)
